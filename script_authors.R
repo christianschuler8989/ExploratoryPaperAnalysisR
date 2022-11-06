d019bec44f4223bfd6b0a1424e43c4eebd5e8b2d
Christian Schuler <90908842+christianschuler8989@users.noreply.github.com>
2022-11-06 10:04:30 +0100

    Add files via upload
    Adding more automation (now based on new example data)

--- script_authors.R
+++ script_authors.R
@@ -23,11 +23,14 @@ data_aut_leg <- data_aut %>% dplyr::filter(IdentifierGroup == "Legends")
 # Publications per author #
 ###########################
 plot_a_publications_ref <- ggplot(data_aut_ref, aes(x= reorder(Name, +Publications), y=Publications)) +
-  ggtitle("Publications per author (from the references of this paper)") +
+  ggtitle("Publications per author (from this paper)") +
   geom_bar(stat='identity', position='dodge', colour="black", aes(color=IdentifierGroup, fill=IdentifierGroup)) +
   scale_fill_manual("Groups", values = c("Reference" = "blue", "Hamburg" = "red", "Legends" = "green")) + 
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1, size=8),
-        legend.position = c(0.2, 0.7)) +
+  theme(axis.text.x = element_text(angle = 45, vjust = 0.8, hjust = 0.5, size=16),
+        axis.text.y = element_text(size=16),
+        legend.position = c(0.15, 0.8), 
+        legend.key.size = unit(2, 'cm'),
+        legend.text = element_text(size=16)) +
   labs(x = "Authors from the reference-section of the current paper")
 #plot_a_publications_ref
 
@@ -35,8 +38,11 @@ plot_a_publications_ham <- ggplot(data_aut_ham, aes(x= reorder(Name, +Publicatio
   ggtitle("Publications per author (from University of Hamburg)") +
   geom_bar(stat='identity', position='dodge', colour="black", aes(color=IdentifierGroup, fill=IdentifierGroup)) +
   scale_fill_manual("Groups", values = c("Reference" = "blue", "Hamburg" = "red", "Legends" = "green")) + 
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1, size=8),
-        legend.position = c(0.2, 0.7)) +
+  theme(axis.text.x = element_text(angle = 45, vjust = 0.8, hjust = 0.5, size=16),
+        axis.text.y = element_text(size=16),
+        legend.position = c(0.15, 0.8), 
+        legend.key.size = unit(2, 'cm'),
+        legend.text = element_text(size=16)) +
   labs(x = "Authors from University Hamburg")
 #plot_a_publications_ham
 
@@ -44,8 +50,11 @@ plot_a_publications_leg <- ggplot(data_aut_leg, aes(x= reorder(Name, +Publicatio
   ggtitle("Publications per author (from legendary scientists of history)") +
   geom_bar(stat='identity', position='dodge', colour="black", aes(color=IdentifierGroup, fill=IdentifierGroup)) +
   scale_fill_manual("Groups", values = c("Reference" = "blue", "Hamburg" = "red", "Legends" = "green")) + 
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1, size=8),
-        legend.position = c(0.2, 0.7)) +
+  theme(axis.text.x = element_text(angle = 45, vjust = 0.8, hjust = 0.5, size=16),
+        axis.text.y = element_text(size=16),
+        legend.position = c(0.15, 0.8), 
+        legend.key.size = unit(2, 'cm'),
+        legend.text = element_text(size=16)) +
   labs(x = "Legendary authors")
 #plot_a_publications_leg
 
@@ -53,8 +62,11 @@ plot_a_publications_all <- ggplot(data_aut, aes(x= reorder(Name, +Publications),
   ggtitle("Publications per author (all of them)") +
   geom_bar(stat='identity', position='dodge', colour="black", aes(color=IdentifierGroup, fill=IdentifierGroup)) +
   scale_fill_manual("Groups", values = c("Reference" = "blue", "Hamburg" = "red", "Legends" = "green")) + 
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1, size=6),
-        legend.position = c(0.2, 0.7)) +
+  theme(axis.text.x = element_text(angle = 45, vjust = 0.8, hjust = 0.5, size=16),
+        axis.text.y = element_text(size=16),
+        legend.position = c(0.15, 0.8), 
+        legend.key.size = unit(2, 'cm'),
+        legend.text = element_text(size=16)) +
   labs(x = "All authors")
 #plot_a_publications_all
 
@@ -63,38 +75,54 @@ plot_a_publications_all <- ggplot(data_aut, aes(x= reorder(Name, +Publications),
 # Citations per author #
 ########################
 plot_a_citations_ref <- ggplot(data_aut_ref, aes(x= reorder(Name, +Citations), y=Citations)) +
-  ggtitle("Citations per author (from the references of this paper)") +
+  ggtitle("Citations per author (from this paper)") +
   geom_bar(stat='identity', position='dodge', colour="black", aes(color=IdentifierGroup, fill=IdentifierGroup)) +
-  scale_fill_manual("Groups", values = c("Reference" = "blue", "Hamburg" = "red", "Legends" = "green")) + 
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1, size=8),
-        legend.position = c(0.2, 0.7)) +
+  scale_y_continuous(labels = label_number()) +
+  scale_fill_manual("Groups", values = c("Reference" = "blue")) + 
+  theme(axis.text.x = element_text(angle = 45, vjust = 0.8, hjust = 0.5, size=16),
+        axis.text.y = element_text(size=16),
+        legend.position = c(0.15, 0.8), 
+        legend.key.size = unit(2, 'cm'),
+        legend.text = element_text(size=16)) +
   labs(x = "Authors from the reference-section of the current paper")
 #plot_a_citations_ref
 
 plot_a_citations_ham <- ggplot(data_aut_ham, aes(x= reorder(Name, +Citations), y=Citations)) +
   ggtitle("Citations per author (from University of Hamburg)") +
   geom_bar(stat='identity', position='dodge', colour="black", aes(color=IdentifierGroup, fill=IdentifierGroup)) +
-  scale_fill_manual("Groups", values = c("Reference" = "blue", "Hamburg" = "red", "Legends" = "green")) + 
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1, size=8),
-        legend.position = c(0.2, 0.7)) +
+  scale_y_continuous(labels = label_number()) +
+  scale_fill_manual("Groups", values = c("Hamburg" = "red")) + 
+  theme(axis.text.x = element_text(angle = 45, vjust = 0.8, hjust = 0.5, size=16),
+        axis.text.y = element_text(size=16),
+        legend.position = c(0.15, 0.8), 
+        legend.key.size = unit(2, 'cm'),
+        legend.text = element_text(size=16)) +
   labs(x = "Author from University Hamburg")
 #plot_a_citations_ham
 
 plot_a_citations_leg <- ggplot(data_aut_leg, aes(x= reorder(Name, +Citations), y=Citations)) +
   ggtitle("Citations per author (from legendary scientists of history)") +
   geom_bar(stat='identity', position='dodge', colour="black", aes(color=IdentifierGroup, fill=IdentifierGroup)) +
-  scale_fill_manual("Groups", values = c("Reference" = "blue", "Hamburg" = "red", "Legends" = "green")) + 
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1, size=8),
-        legend.position = c(0.2, 0.7)) +
+  scale_y_continuous(labels = label_number()) +
+  scale_fill_manual("Groups", values = c("Legends" = "green")) + 
+  theme(axis.text.x = element_text(angle = 45, vjust = 0.8, hjust = 0.5, size=16),
+        axis.text.y = element_text(size=16),
+        legend.position = c(0.15, 0.8), 
+        legend.key.size = unit(2, 'cm'),
+        legend.text = element_text(size=16)) +
   labs(x = "Legendary authors")
 #plot_a_citations_leg
 
 plot_a_citations_all <- ggplot(data_aut, aes(x= reorder(Name, +Citations), y=Citations)) +
   ggtitle("Citations per author (all of them)") +
   geom_bar(stat='identity', position='dodge', colour="black", aes(color=IdentifierGroup, fill=IdentifierGroup)) +
+  scale_y_continuous(labels = label_number()) +
   scale_fill_manual("Groups", values = c("Reference" = "blue", "Hamburg" = "red", "Legends" = "green")) + 
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1, size=6),
-        legend.position = c(0.2, 0.7)) +
+  theme(axis.text.x = element_text(angle = 45, vjust = 0.8, hjust = 0.5, size=16),
+        axis.text.y = element_text(size=16),
+        legend.position = c(0.15, 0.8), 
+        legend.key.size = unit(2, 'cm'),
+        legend.text = element_text(size=16)) +
   labs(x = "All authors")
 #plot_a_citations_all
 
@@ -103,11 +131,14 @@ plot_a_citations_all <- ggplot(data_aut, aes(x= reorder(Name, +Citations), y=Cit
 # H-Index per author #
 ######################
 plot_a_hindex_ref <- ggplot(data_aut_ref, aes(x= reorder(Name, +Hindex), y=Hindex)) +
-  ggtitle("H-Index per author (from the references of this paper)") +
+  ggtitle("H-Index per author (from this paper)") +
   geom_bar(stat='identity', position='dodge', colour="black", aes(color=IdentifierGroup, fill=IdentifierGroup)) +
   scale_fill_manual("Groups", values = c("Reference" = "blue", "Hamburg" = "red", "Legends" = "green")) + 
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1, size=8),
-        legend.position = c(0.2, 0.7)) +
+  theme(axis.text.x = element_text(angle = 45, vjust = 0.8, hjust = 0.5, size=16),
+        axis.text.y = element_text(size=16),
+        legend.position = c(0.15, 0.8), 
+        legend.key.size = unit(2, 'cm'),
+        legend.text = element_text(size=16)) +
   labs(x = "Authors from the reference-section of the current paper")
 #plot_a_hindex_ref
 
@@ -115,8 +146,11 @@ plot_a_hindex_ham <- ggplot(data_aut_ham, aes(x= reorder(Name, +Hindex), y=Hinde
   ggtitle("H-Index per author (from University of Hamburg)") +
   geom_bar(stat='identity', position='dodge', colour="black", aes(color=IdentifierGroup, fill=IdentifierGroup)) +
   scale_fill_manual("Groups", values = c("Reference" = "blue", "Hamburg" = "red", "Legends" = "green")) + 
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1, size=8),
-        legend.position = c(0.2, 0.7)) +
+  theme(axis.text.x = element_text(angle = 45, vjust = 0.8, hjust = 0.5, size=16),
+        axis.text.y = element_text(size=16),
+        legend.position = c(0.15, 0.8), 
+        legend.key.size = unit(2, 'cm'),
+        legend.text = element_text(size=16)) +
   labs(x = "Author from University Hamburg")
 #plot_a_hindex_ham
 
@@ -124,8 +158,11 @@ plot_a_hindex_leg <- ggplot(data_aut_leg, aes(x= reorder(Name, +Hindex), y=Hinde
   ggtitle("H-Index per author (from legendary scientists of history)") +
   geom_bar(stat='identity', position='dodge', colour="black", aes(color=IdentifierGroup, fill=IdentifierGroup)) +
   scale_fill_manual("Groups", values = c("Reference" = "blue", "Hamburg" = "red", "Legends" = "green")) + 
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1, size=8),
-        legend.position = c(0.2, 0.7)) +
+  theme(axis.text.x = element_text(angle = 45, vjust = 0.8, hjust = 0.5, size=16),
+        axis.text.y = element_text(size=16),
+        legend.position = c(0.15, 0.8), 
+        legend.key.size = unit(2, 'cm'),
+        legend.text = element_text(size=16)) +
   labs(x = "Legendary authors")
 #plot_a_hindex_leg
 
@@ -133,8 +170,11 @@ plot_a_hindex_all <- ggplot(data_aut, aes(x= reorder(Name, +Hindex), y=Hindex)) 
   ggtitle("H-Index per author (all of them)") +
   geom_bar(stat='identity', position='dodge', colour="black", aes(color=IdentifierGroup, fill=IdentifierGroup)) +
   scale_fill_manual("Groups", values = c("Reference" = "blue", "Hamburg" = "red", "Legends" = "green")) + 
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1, size=6),
-        legend.position = c(0.2, 0.7)) +
+  theme(axis.text.x = element_text(angle = 45, vjust = 0.8, hjust = 0.5, size=16),
+        axis.text.y = element_text(size=16),
+        legend.position = c(0.15, 0.8), 
+        legend.key.size = unit(2, 'cm'),
+        legend.text = element_text(size=16)) +
   labs(x = "All authors")
 #plot_a_hindex_all
 
@@ -142,53 +182,56 @@ plot_a_hindex_all <- ggplot(data_aut, aes(x= reorder(Name, +Hindex), y=Hindex)) 
 ############################################
 # Saving plots as png in current directory #
 ############################################
-png(file="paper03-authors-publications-reference.png",
+png(file="authors-publications-reference.png",
     height=800, width=800)
 plot_a_publications_ref
 dev.off()
-png(file="paper03-authors-publications-hamburg.png",
+png(file="authors-publications-hamburg.png",
     height=800, width=800)
 plot_a_publications_ham
 dev.off()
-png(file="paper03-authors-publications-legends.png",
+png(file="authors-publications-legends.png",
     height=800, width=800)
 plot_a_publications_leg
 dev.off()
-png(file="paper03-authors-publications-all.png",
+png(file="authors-publications-all.png",
     height=800, width=800)
 plot_a_publications_all
 dev.off()
 
-png(file="paper03-authors-citations-reference.png",
+png(file="authors-citations-reference.png",
     height=800, width=800)
 plot_a_citations_ref
 dev.off()
-png(file="paper03-authors-citations-hamburg.png",
+png(file="authors-citations-hamburg.png",
     height=800, width=800)
 plot_a_citations_ham
 dev.off()
-png(file="paper03-authors-citations-legends.png",
+png(file="authors-citations-legends.png",
     height=800, width=800)
 plot_a_citations_leg
 dev.off()
-png(file="paper03-authors-citations-all.png",
+png(file="authors-citations-all.png",
     height=800, width=800)
 plot_a_citations_all
 dev.off()
 
-png(file="paper03-authors-hindex-reference.png",
+png(file="authors-hindex-reference.png",
     height=800, width=800)
 plot_a_hindex_ref
 dev.off()
-png(file="paper03-authors-hindex-hamburg.png",
+png(file="authors-hindex-hamburg.png",
     height=800, width=800)
 plot_a_hindex_ham
 dev.off()
-png(file="paper03-authors-hindex-legends.png",
+png(file="authors-hindex-legends.png",
     height=800, width=800)
 plot_a_hindex_leg
 dev.off()
-png(file="paper03-authors-hindex-all.png",
+png(file="authors-hindex-all.png",
     height=800, width=800)
 plot_a_hindex_all
 dev.off()
+
+################################################################################
+setwd(dir_init)
--- script_references.R
+++ script_references.R
@@ -16,42 +16,183 @@
 # • Other “gray” literature (Websites, Wikipedia, Etc.)
 # c("Journal", "Conference", "Workshop", "Magazine", "Book", "Technical Report", "Dissertation", "Master Thesis", "Wikipedia", "Website")
 
-#################
-# Some counting #
-#################
-data_ref_doi <- data_ref %>% group_by(OfficialDoi) %>%
-  summarise(OfficialDoiCount = n())
 
+visualiseCitations <- function(dataframe, paperidentifier, imgAuthor) {
+  ################################################
+  # Regarding the Maturity of referenced Sources #
+  ################################################
+  # Selecting specific variables to include in new dataframe
+  dataframe_off <- dataframe %>% select(Item.Type, Publication.Title, Marker)
+  # Sorting in descending order of "Maturity"
+  # Automatic-based categorization for all papers:
+  dataframe_off <- dataframe_off %>% arrange(factor(Item.Type, levels = c("journalArticle", "conferencePaper", "book", "bookSection", "report", "document")))
+  # Manual categorization as done for our first main-paper:
+  #data_ref_off <- data_ref_off %>% arrange(factor(Maturity, levels = c("Journal", "Online Journal", "Conference", "Workshop", "Magazine", "Book", "Technical Report", "Dissertation", "Master Thesis", "Wikipedia", "Website")))
+  
+  # Print tables to a png file:
+  png(file=paste0(paperidentifier, "-references-official.png"),
+      height = 25*nrow(dataframe_off),
+      width = 300*ncol(dataframe_off))
+  grid.table(dataframe_off)
+  dev.off()
+  
+  ################################################################
+  # Handling cases without a citation-count (no citations found) #
+  ################################################################
+  dataframe$SemanticScholarCitations[dataframe$SemanticScholarCitations == "No Citations Found"] <- 0
+  dataframe$SemanticScholarCitations = as.numeric(as.character(dataframe$SemanticScholarCitations))
+  #data_ref_all$SemanticScholarCitations[is.character(data_ref_all$SemanticScholarCitations)] <- as.numeric(0)
+  
+  dataframe$CrossRefCitations[dataframe$CrossRefCitations == "No Citations Found"] <- 0
+  dataframe$CrossRefCitations = as.numeric(as.character(dataframe$CrossRefCitations))
+  #data_ref_all$CrossRefCitations[is.character(data_ref_all$CrossRefCitations)] <- as.numeric(0)
+  
+  dataframe_temp <- dataframe %>% select(Marker, Item.Type, Publication.Title, SemanticScholarCitations, CrossRefCitations)
+  
+  ##############################################################
+  # Citations per referenced scientific work (SemanticScholar) #
+  ##############################################################
+  plot_r_citations_semanticScholar <- ggplot(dataframe_temp, aes(x= reorder(Marker, +SemanticScholarCitations), y=SemanticScholarCitations)) +
+    ggtitle("Citations (SemanticScholar) per scientific work (from the references of this paper)") +
+    geom_bar(stat='identity', position='dodge', colour="black", aes(color=Item.Type, fill=Item.Type)) +
+    #scale_fill_manual("Category", values = c("Author" = "red", "Other" = "blue")) + 
+    theme(axis.text.x = element_text(size=6),
+          legend.position = c(0.2, 0.8)) + 
+    labs(x = "Identifier based on reference in this paper") +
+    # Insert image of author to the plot
+    inset_element(p = imgAuthor,
+                   left = 0.4,
+                   bottom = 0.6,
+                   right = 0.9,
+                   top = 0.9)
+  print("SemanticScholarPlot")
+  #plot_r_citations_semanticScholar
+  ############################################
+  # Saving plots as png in current directory #
+  ############################################
+  png(file=paste0(paperidentifier, "-citationsSemanticScholar.png"))
+  print(plot_r_citations_semanticScholar)
+  dev.off()
+  
+  
+  #######################################################
+  # Citations per referenced scientific work (CrossRef) #
+  #######################################################
+  plot_r_citations_crossRef <- ggplot(dataframe_temp, aes(x= reorder(Marker, +CrossRefCitations), y=CrossRefCitations)) +
+    ggtitle("Citations (CrossRef) per scientific work (from the references of this paper)") +
+    geom_bar(stat='identity', position='dodge', colour="black", aes(color=Item.Type, fill=Item.Type)) +
+    #scale_fill_manual("Category", values = c("Author" = "red", "Other" = "blue")) + 
+    theme(axis.text.x = element_text(size=6),
+          legend.position = c(0.2, 0.8)) + 
+    labs(x = "Identifier based on reference in this paper")
+  print("CrossRefPlot")
+  #plot_r_citations_crossRef
+  ############################################
+  # Saving plots as png in current directory #
+  ############################################
+  png(file=paste0(paperidentifier, "-citationsCrossRef.png"))
+  print(plot_r_citations_crossRef)
+  dev.off()
+
+}
+
+
+#################################
+# Function calls for each paper #
+#################################
+#visualiseCitations(data_ref_all, "MainPapers")
+visualiseCitations(data_ref_p00_schuler, "Schuler", img_schuler)
+visualiseCitations(data_ref_p01_sturm, "Sturm", img_sturm)
+visualiseCitations(data_ref_p02_lebovitz, "Lebovitz", img_lebovitz)
+visualiseCitations(data_ref_p03_kane, "Kane", img_kane)
+
+
+setwd(dir_init)
+
+
+
+################################################################################
+
+################################################################################
+
+################################################################################
+
+################################################################################
+# Manual (prior and outdated) parts:
+#
+
+
+
+################################################
+# Regarding the Maturity of referenced Sources #
+################################################
 # Selecting specific variables to include in new dataframe
-data_ref_off <- data_ref %>% select(Marker, Maturity, Publisher, PublisherName)
+data_ref_off <- data_ref_all %>% select(Item.Type, Publication.Title, Marker)
 # Sorting in descending order of "Maturity"
-data_ref_off <- data_ref_off %>% arrange(factor(Maturity, levels = c("Journal", "Online Journal", "Conference", "Workshop", "Magazine", "Book", "Technical Report", "Dissertation", "Master Thesis", "Wikipedia", "Website")))
+# Automatic-based categorization for all papers:
+data_ref_off <- data_ref_off %>% arrange(factor(Item.Type, levels = c("journalArticle", "conferencePaper", "book", "bookSection", "report", "document")))
+# Manual categorization as done for our first main-paper:
+#data_ref_off <- data_ref_off %>% arrange(factor(Maturity, levels = c("Journal", "Online Journal", "Conference", "Workshop", "Magazine", "Book", "Technical Report", "Dissertation", "Master Thesis", "Wikipedia", "Website")))
+
+# Print tables to a png file:
+png(file="all-references-official.png",
+    height = 25*nrow(data_ref_off),
+    width = 300*ncol(data_ref_off))
+grid.table(data_ref_off)
+dev.off()
+
+################################################################
+# Handling cases without a citation-count (no citations found) #
+################################################################
+data_ref_all$SemanticScholarCitations[data_ref_all$SemanticScholarCitations == "No Citations Found"] <- 0
+data_ref_all$SemanticScholarCitations = as.numeric(as.character(data_ref_all$SemanticScholarCitations))
+#data_ref_all$SemanticScholarCitations[is.character(data_ref_all$SemanticScholarCitations)] <- as.numeric(0)
+
+data_ref_all$CrossRefCitations[data_ref_all$CrossRefCitations == "No Citations Found"] <- 0
+data_ref_all$CrossRefCitations = as.numeric(as.character(data_ref_all$CrossRefCitations))
+#data_ref_all$CrossRefCitations[is.character(data_ref_all$CrossRefCitations)] <- as.numeric(0)
 
 
-############################################
-# Citations per referenced scientific work #
-############################################
-plot_r_citations <- ggplot(data_ref, aes(x= reorder(Marker, +Citations), y=Citations)) +
-  ggtitle("Citations per scientific work (from the references of this paper)") +
-  geom_bar(stat='identity', position='dodge', colour="black", aes(color=Category, fill=Category)) +
-  scale_fill_manual("Category", values = c("Author" = "red", "Other" = "blue")) + 
+
+##############################################################
+# Citations per referenced scientific work (SemanticScholar) #
+##############################################################
+plot_r_citations_semanticScholar <- ggplot(data_ref_all, aes(x= reorder(Marker, +SemanticScholarCitations), y=SemanticScholarCitations)) +
+  ggtitle("Citations (SemanticScholar) per scientific work (from the references of this paper)") +
+  geom_bar(stat='identity', position='dodge', colour="black", aes(color=Item.Type, fill=Item.Type)) +
+  #scale_fill_manual("Category", values = c("Author" = "red", "Other" = "blue")) + 
   theme(axis.text.x = element_text(size=8),
         legend.position = c(0.1, 0.8)) + 
   labs(x = "Identifier based on reference in this paper")
-plot_r_citations
+plot_r_citations_semanticScholar
+############################################
+# Saving plots as png in current directory #
+############################################
+png(file="references-citations-semanticScholar.png")
+plot_r_citations_sementicScholar
+dev.off()
 
 
-########################################
-# Views per referenced scientific work #
-########################################
-plot_r_views <- ggplot(data_ref, aes(x= reorder(Marker, +Views), y=Views)) +
-  ggtitle("Views per scientific work (from the references of this paper)") +
-  geom_bar(stat='identity', position='dodge', colour="black", aes(color=Category, fill=Category)) +
-  scale_fill_manual("Category", values = c("Author" = "red", "Other" = "blue")) + 
+#######################################################
+# Citations per referenced scientific work (CrossRef) #
+#######################################################
+plot_r_citations_crossRef <- ggplot(data_ref_all, aes(x= reorder(Marker, +CrossRefCitations), y=CrossRefCitations)) +
+  ggtitle("Citations (CrossRef) per scientific work (from the references of this paper)") +
+  geom_bar(stat='identity', position='dodge', colour="black", aes(color=Item.Type, fill=Item.Type)) +
+  #scale_fill_manual("Category", values = c("Author" = "red", "Other" = "blue")) + 
   theme(axis.text.x = element_text(size=8),
         legend.position = c(0.1, 0.8)) + 
   labs(x = "Identifier based on reference in this paper")
-plot_r_views
+plot_r_citations_crossRef
+############################################
+# Saving plots as png in current directory #
+############################################
+png(file="references-citations-crossRef.png")
+plot_r_citations_crossRef
+dev.off()
+
+
+
 
 
 #########################################
@@ -67,27 +208,13 @@ plot_r_publishers <- ggplot(data_ref_doi, aes(x= reorder(OfficialDoi, +OfficialD
   #labs(x = "Publisher of referenced work")
   labs(x = "Identifier based on reference in this paper")
 plot_r_publishers
-
-
 ############################################
 # Saving plots as png in current directory #
 ############################################
-png(file="paper03-references-citations.png")
-plot_r_citations
-dev.off()
-
-png(file="paper03-references-views.png")
-plot_r_views
-dev.off()
-
-png(file="paper03-references-publishers.png")
+png(file="references-publishers.png")
 plot_r_publishers
 dev.off()
 
-# Print tables to a png file:
-png(file="paper03-references-official.png",
-    height = 25*nrow(data_ref_off),
-    width = 300*ncol(data_ref_off))
-grid.table(data_ref_off)
-dev.off()
+
+
 
